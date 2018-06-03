c4ae8b750fa31bdc223aa4bac3f229fab74d5ce3
max <max@localhost.localdomain>
2018-06-03 13:35:23 +0200

    material ui

--- whatever/src/App.js
+++ whatever/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: deepOrange,
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider theme={darkTheme}>
+        <div className="App">
+        </div>
+      </MuiThemeProvider>
+      
     );
   }
 }
